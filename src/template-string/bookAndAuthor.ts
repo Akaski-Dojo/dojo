2ba81b164d915a483aca96c10d2538ade4d48dda
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-07-14 16:59:29 -0500

    changes in the document bookAndAuthor.ts

--- src/template-string/bookAndAuthor.ts
+++ src/template-string/bookAndAuthor.ts
@@ -1,5 +1,11 @@
 function bookAndAuthor(bookName: string, authorName: string, datePublished: { month: string; day: number; year: number }): string {
-  return ''
+  return `---\n${bookName}\nby ${authorName}\n\n  Published: ${datePublished.month} ${datePublished.day}, ${datePublished.year}\n---`
 }
+
+console.log(bookAndAuthor(
+  'Narnia la continuación, 100 años después del accidente',
+  'Leonardo Jose Saturno Suarez',
+  { month: 'Septiembre', day: 7, year: 1999 },
+))
 
 export { bookAndAuthor }
