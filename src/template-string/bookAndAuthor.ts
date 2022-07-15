3097b06557a3cea1fe9195d26149364985f1b742
Kaution <105989497+LS-Kaution@users.noreply.github.com>
2022-07-15 12:52:08 -0500

    kata/bookAndAuthor (#1)
    * changes in the document bookAndAuthor.ts
    
    * add: new rule for bookAuthor kata
    
    * New actualization of document bookAndAuthor.ts
    
    Co-authored-by: René <hola@renejfc.dev>

--- src/template-string/bookAndAuthor.ts
+++ src/template-string/bookAndAuthor.ts
@@ -1,5 +1,16 @@
 function bookAndAuthor(bookName: string, authorName: string, datePublished: { month: string; day: number; year: number }): string {
-  return ''
+  return `---
+${bookName}
+by ${authorName}
+
+  Published: ${datePublished.month} ${datePublished.day}, ${datePublished.year}
+---`
 }
+
+console.log(bookAndAuthor(
+  'Narnia la continuación, 100 años después del accidente',
+  'Leonardo Jose Saturno Suarez',
+  { month: 'Septiembre', day: 7, year: 1999 },
+))
 
 export { bookAndAuthor }
